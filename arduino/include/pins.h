8876ddc27b9ddc0f13b854e2ba2a2cf51b1e71a4
Ace-2000 <111930953+Ace-2000@users.noreply.github.com>
2025-02-26 23:47:28 -0500

    Create pins.h

---
+++ arduino/include/pins.h
@@ -0,0 +1,10 @@
+#ifndef PINS_H
+#define PINS_H
+
+// --- Potentiometer ---
+#define POT_PIN A0            // Potentiometer input
+
+// --- DS18B20 Temperature Sensor ---
+#define ONE_WIRE_BUS 2        // Data wire for DS18B20
+
+#endif // PINS_H
