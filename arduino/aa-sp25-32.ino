e398a6504428700b5652bec587a92a9db87c9fb2
Zachary Chu <chuzacharyj@vt.edu>
2025-03-05 10:37:10 -0500

    Testing partial implementation
    - UNO R3 generates messages for the MKR1500 to decode.

--- arduino/aa-sp25-32.ino
+++ arduino/aa-sp25-32.ino
@@ -1,16 +1,120 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+#define MODE_PIN 11
+#define SEND_PIN 12
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+bool mode = true;
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(19200, SERIAL_8N1);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;  // Don't proceed, loop forever, absolutely borked
+    }
+
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+
+    pinMode(MODE_PIN, INPUT_PULLUP);
+    pinMode(SEND_PIN, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    digitalWrite(LED_BUILTIN, mode);
+
+    Serial.begin(19200, SERIAL_8N1);
 }
 
+unsigned long time = 0;
+bool flag = false;
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  String data = "\r\nV\tThis is a sample message.";
-  int count = 0;
-  while (Serial.available()) {
-    for (int i = 0; i < data.length(); i++) {
-      Serial.write(data[i]);
+    modeButton();
+    dataButton();
+}
+
+void modeButton() {
+    // swaps the message type from Victron to Renogy stucture.
+    static unsigned long debounceTime = 0;
+    unsigned long wait = 20;
+
+    static int ledState = HIGH;
+    static int buttonState;
+    static int lastButtonState = LOW;
+
+    int reading = digitalRead(MODE_PIN);
+
+    if (reading != lastButtonState) {
+        // reset the debouncing timer
+        debounceTime = millis();
     }
-    count++;
-  }
+
+    if ((millis() - debounceTime) > wait) {
+        if (reading != buttonState) {
+            buttonState = reading;
+            if (buttonState == HIGH) {
+                ledState = !ledState;
+                mode = !mode;
+            }
+        }
+    }
+    digitalWrite(LED_BUILTIN, ledState);
+    lastButtonState = reading;
+}
+
+void dataButton() {
+    // sends a random generated serial message.
+    static unsigned long debounceTime = 0;
+    unsigned long wait = 20;
+
+    static int ledState = HIGH;
+    static int buttonState;
+    static int lastButtonState = LOW;
+
+    int reading = digitalRead(SEND_PIN);
+
+    if (reading != lastButtonState) {
+        debounceTime = millis();
+    }
+
+    if ((millis() - debounceTime) > wait) {
+        if (reading != buttonState) {
+            buttonState = reading;
+            if (buttonState == HIGH) {
+                writeData(display, mode);
+            }
+        }
+    }
+    lastButtonState = reading;
+}
+
+void writeData(Adafruit_SSD1306& display, bool mode) {
+    // write data to serial out.
+    // mode will determine baud and message structure.
+
+    randomSeed(analogRead(0));
+    double num;
+    if (mode == false) {
+        num = random(1100, 1400) / 100.0;
+    }
+    else {
+        num = random(10000, 40000) / 100.0;
+    }
+    char val[50];
+    dtostrf(num, 5, 2, val);  // sprintf doesn't support %f.
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.write(val);
+    display.display();
 }
