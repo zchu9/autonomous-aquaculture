53da35d350123119ba5d5cd904b4a510e0d73694
Zachary Chu <chuzacharyj@vt.edu>
2025-03-05 16:03:57 -0500

    Message formatting for spoofed messages.

--- arduino/aa-sp25-32.ino
+++ arduino/aa-sp25-32.ino
@@ -1,3 +1,6 @@
+// 3.04.2025
+// Arduino Uno R3 driver for generating data and sending it over UART for MKR1500.
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -104,17 +107,49 @@ void writeData(Adafruit_SSD1306& display, bool mode) {
     // mode will determine baud and message structure.
 
     randomSeed(analogRead(0));
-    double num;
+    byte num;
     if (mode == false) {
-        num = random(1100, 1400) / 100.0;
+        num = random(1100, 1400) / 10.0; // victron format.
     }
     else {
-        num = random(10000, 40000) / 100.0;
+        num = random(1000, 4000) / 10.0;  // renogy mAh format.
     }
-    char val[50];
-    dtostrf(num, 5, 2, val);  // sprintf doesn't support %f.
+    char val[10];
+    dtostrf(num, 5, 2, val);  // sprintf doesn't support %f on lib-avr.
     display.clearDisplay();
     display.setCursor(0, 0);
     display.write(val);
     display.display();
+
+    char buffer[50];
+    if (mode == false) {
+        // victron
+        snprintf(buffer, 50, "V\t%s\r\n", val);
+
+        // switch.
+        Serial.flush();
+        Serial.begin(19200, SERIAL_8N1);
+        while (Serial.available()) {
+            Serial.read();
+        }
+        Serial.write(buffer);
+    }
+    else {
+        // renogy
+        byte high, low;
+        high = ((num >> 8) & 0xFF);
+        low = (num & 0xFF);
+
+        // switch.
+        Serial.flush();
+        Serial.begin(9600, SERIAL_8E1);
+        while (Serial.available()) {
+            Serial.read();
+        }
+        for (int i = 0; i < 3; i++) {
+            // since the bytes are discarded on MKR1500, just send anything.
+            Serial.write(high);
+            Serial.write(low);
+        }
+    }
 }
