fb0846167e14ee85835be0a33a6370f0db125ad4
Zachary Chu <chuzacharyj@vt.edu>
2025-03-07 10:40:17 -0500

    Placeholder renogy file.

---
+++ MCU/lib/FSM/renogy.h
@@ -0,0 +1,34 @@
+// 3.01.2025
+// placeholder header for the Rover MPPT data
+// Renogy Modbus RTU Protocol
+
+// format:
+// start . address . function . data    . error check . end
+// 10ms  . 1 byte  . 1 byte   . 2 bytes . 2 byte   . 10ms
+
+#include "crc.h"
+
+#define renogyBaud 9600
+#define renogyConfig SERIAL_8E1 // modbus *requires* even parity or 2 stop bits.
+
+// to be expanded with additional function codes from ROVER_MODBUS.PDF
+typedef enum {
+    panelV = 0x0107,
+    panelA = 0x0108,
+    panelW = 0x0109,
+} funcCodes;
+
+struct renogyDevice
+{
+    unit16_t deviceAddress;
+};
+
+
+void getDeviceAddress();
+void readRenogyPowerData() {
+    // validate correct line and protocol
+    // send address + function
+    // wait, recieve bytes, crc.add(b)
+    // crc validate
+    // repeat for each stat as needed.
+};
