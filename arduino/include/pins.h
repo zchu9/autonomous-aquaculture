97c6ff39d934a2cac21521a158d4fb506678236d
Ace-2000 <111930953+Ace-2000@users.noreply.github.com>
2025-03-17 13:23:28 -0400

    Update pins.h
    Added Camera

--- arduino/include/pins.h
+++ arduino/include/pins.h
@@ -7,4 +7,7 @@
 // --- DS18B20 Temperature Sensor ---
 #define ONE_WIRE_BUS 2        // Data wire for DS18B20
 
+// --- ArduCam Mini 2MP Camera ---
+#define CAMERA_CS_PIN 7       // Chip Select pin for the camera
+
 #endif // PINS_H
