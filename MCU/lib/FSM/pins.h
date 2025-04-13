3ccfa2d09cdaa7c6cca82a38cb50bd696b8a3d54
Zachary Chu <chuzacharyj@vt.edu>
2025-04-13 12:55:52 -0400

    doestn work still

--- MCU/lib/FSM/pins.h
+++ MCU/lib/FSM/pins.h
@@ -2,6 +2,9 @@
 #define MUX_SEL_1 1
 #define MUX_SEL_2 2
 
+#define MUX_ENABLE_0 6
+#define MUX_ENABLE_1 7
+
 #define MUX_DISABLE_0 3 // RX
 #define MUX_DISABLE_1 4 // TX
 #define MUX_DISABLE_2 5 // Winch
