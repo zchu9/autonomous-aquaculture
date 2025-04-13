2374e6f2ec154e6b3f063e222425959b17c6ce6b
Zachary Chu <chuzacharyj@vt.edu>
2025-04-13 10:28:59 -0400

    dropped Renogy

--- MCU/lib/Power/renogy.h
+++ MCU/lib/Power/renogy.h
@@ -3,40 +3,6 @@
 #include <Arduino.h>
 #include <ModbusMaster.h>
 
-void getDeviceAddress() {
-    // modbus devices have an address between 1 and 256; 0 is global broadcast.
-    // polling all devices may be necessary to find our device. this function is time consuming.
-    uint8_t startAddress = 0x01;
-    uint8_t endAddress = 0xFF;
-
-    // modbus message; attempts to read system voltage/current at 0x000A.
-    const size_t message_size = 8;
-    uint8_t message[message_size] = { '\0', READREG, 0, 0xA, 0, 1, '\0', '\0' };
-    char serialMessage[message_size];
-
-    // TODO: configure UART
-    // then send to each address and check for a response.
-    for (uint8_t i = startAddress; i < endAddress; i++) {
-        message[0] = i; // address
-        int crc = genCRC(message, 6);
-        message[6] = crc & 0xFF;    // crc low
-        message[7] = (crc >> 8) & 0xFF; // crc high
-        memcpy(serialMessage, message, sizeof(uint8_t));    // u_char cannot be cast to char; this avoids cast errors.
-        while (Serial1.available() > 0) {
-            Serial1.read(); // clear input buffer.
-        }
-        Serial1.write(serialMessage);
-
-        delay(RENOGY_DELAY);    // some arbitrary delay to ensure a response from the device.
-
-        if (Serial1.available() > 0) {
-            // TODO: if there's a response, check it's validity then return address i;
-            // otherwise move to the next address.
-        }
-
-    }
-};
-
 // A struct to hold the controller data
 struct Controller_data {
 
