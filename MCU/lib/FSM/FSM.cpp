a08e22b0754fcb7e43d1002f05d39142ff97becc
Zachary Chu <chuzacharyj@vt.edu>
2025-04-20 15:58:13 -0400

    Moved serial init

--- MCU/lib/FSM/FSM.cpp
+++ MCU/lib/FSM/FSM.cpp
@@ -32,13 +32,17 @@ void initializeStartup(data &d)
 #endif
 
     attachInterrupt(digitalPinToInterrupt(RX_INTERRUPT), commsHandler, RISING);
-
+    
     // init the heap pointers (jayson said that we should do this)
     d.powerData = new powerInfo;
     d.lora = new LoraRadio;
     d.winch = new winchData(LIFT_PIN, LOWER_PIN, A0);
     d.cam = new CameraHandler;
     d.powerData = new powerInfo;
+
+    Serial1.begin(9600, SERIAL_8N1);
+    timerInit(); 
+    
     d.cam->begin();
     noConnectionMode = d.lora->sendHandshake();
 }
@@ -49,8 +53,6 @@ void initializeStartup(data &d)
 void initializeDebug()
 {
     Serial.begin(9600);
-    Serial1.begin(9600, SERIAL_8N1);
-    timerInit(); // should be a normal startup component;
     Serial.println("Debugging Initialized");
 }
 
