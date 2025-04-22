41b00617e4dd852bb94cbbd3721c0400c3510d89
daniel <dtr@vt.edu>
2025-04-22 14:04:37 -0400

    serial flush was killing iteslf (outch)

--- MCU/lib/Power/uartSwitch.cpp
+++ MCU/lib/Power/uartSwitch.cpp
@@ -14,25 +14,20 @@ void initMuxPins()
 
 void uartSwitch(device dev, long baud, uint16_t config)
 {
-    Serial.print("2");
     // disable the mux, select new lines.
     digitalWrite(MUX_DISABLE_0, HIGH);
     digitalWrite(MUX_DISABLE_1, HIGH);
-    Serial.println("3");
 
-    Serial1.flush(); // Wait for tx to clear.
-    Serial.println("4");
+    // Serial1.flush();             // Wait for tx to clear.
     Serial1.begin(baud, config); // change baud.
-    Serial.println("5");
-    // while (Serial1.available())
-    // {
-    //     Serial1.read(); // clear input buffer.
-    // }
+    while (Serial1.available())
+    {
+        Serial1.read(); // clear input buffer.
+    }
 
     digitalWrite(MUX_SEL_0, (dev & 0x001));
     digitalWrite(MUX_SEL_1, (dev & 0x010));
     digitalWrite(MUX_SEL_2, (dev & 0x100)); // ancillary in this case, we won't have this many lines.
-
-    digitalWrite(MUX_DISABLE_0, LOW); // rx
-    digitalWrite(MUX_DISABLE_1, LOW); // tx
+    digitalWrite(MUX_DISABLE_0, LOW);       // rx
+    digitalWrite(MUX_DISABLE_1, LOW);       // tx
 }
