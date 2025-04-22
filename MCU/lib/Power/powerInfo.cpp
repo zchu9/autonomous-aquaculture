6ea03734e33be3f5fdd3d4b7dc193717d288e5ea
daniel <dtr@vt.edu>
2025-04-22 11:32:20 -0400

    Num checks the cases that call stoi. - gra

--- MCU/lib/Power/powerInfo.cpp
+++ MCU/lib/Power/powerInfo.cpp
@@ -69,61 +69,79 @@ void powerInfo::formatVictronData()
             continue;
         }
 
-        if (this->bms.labels[i][0] == 'H' && checkFieldNum(i))
+        if (checkFieldNum(i))
         {
-            Serial.print("Made it seeyuhhhh 2 ");
-            Serial.println(i);
-            hStatsVictron(i);
+            if (this->bms.labels[i][0] == 'H')
+            {
+                Serial.print("Made it seeyuhhhh 2 ");
+                Serial.println(i);
+                hStatsVictron(i);
+            }
+            else
+            {
+                switch (this->bms.labels[i][0])
+                {
+                case 'V':
+                    this->bms.mvoltage = stoi(this->bms.fields[i]);
+                    break;
+                case 'I':
+                    this->bms.mcurrent = stoi(this->bms.fields[i]);
+                    break;
+                case 'P':
+                    this->bms.power = stoi(this->bms.fields[i]);
+                    break;
+                case 'C':
+                    this->bms.consumedmAH = stoi(this->bms.fields[i]);
+                    break;
+                case 'S':
+                    this->bms.stateOfCharge = stoi(this->bms.fields[i]);
+                    break;
+                case 'T':
+                    this->bms.timeToGo = stoi(this->bms.fields[i]);
+                    break;
+                case 'A':
+                    if (this->bms.labels[i][1] == 'R')
+                    {
+                        this->bms.alarmReason = stoi(this->bms.fields[i]);
+                    }
+                case 'F':
+                    this->bms.firmware = stoi(this->bms.fields[i]);
+                    break;
+                case 'M':
+                    this->bms.monitorMode = stoi(this->bms.fields[i]);
+                    break;
+
+                default:
+                    break;
+                }
+            }
         }
         else
         {
             switch (this->bms.labels[i][0])
             {
-            case 'V':
-                this->bms.mvoltage = stoi(this->bms.fields[i]);
-                break;
-            case 'I':
-                this->bms.mcurrent = stoi(this->bms.fields[i]);
-                break;
-            case 'P':
-                this->bms.power = stoi(this->bms.fields[i]);
-                break;
-            case 'C':
-                this->bms.consumedmAH = stoi(this->bms.fields[i]);
-                break;
-            case 'S':
-                this->bms.stateOfCharge = stoi(this->bms.fields[i]);
-                break;
-            case 'T':
-                this->bms.timeToGo = stoi(this->bms.fields[i]);
-                break;
-            case 'B':
-                this->bms.model = this->bms.fields[i];
-                break;
             case 'A':
                 if (this->bms.labels[i][1] == 'R')
                 {
-                    this->bms.alarmReason = stoi(this->bms.fields[i]);
+                    break;
                 }
                 else
                 {
                     this->bms.alarm = (this->bms.fields[i][1] == 'F') ? false : true;
                 }
                 break;
-            case 'F':
-                this->bms.firmware = stoi(this->bms.fields[i]);
-                break;
-            case 'M':
-                this->bms.monitorMode = stoi(this->bms.fields[i]);
+            case 'B':
+                this->bms.model = this->bms.fields[i];
                 break;
             default:
                 break;
+                // put the switch case with the ones that can take strings.
             }
+            Serial.print("Made it seeyuhhhh 3 ");
+            Serial.println(i);
         }
-        Serial.print("Made it seeyuhhhh 3 ");
-        Serial.println(i);
     }
-};
+}
 
 // collect power data from each source, format it as needed.
 // handles all serial line switching, as needed.
