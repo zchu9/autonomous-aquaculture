d144a2b4ef2ece7b7bdb887c4649bacbc889ac4d
daniel <dtr@vt.edu>
2025-04-30 11:53:50 -0400

    Removed more test code from main

--- MCU/src/main.cpp
+++ MCU/src/main.cpp
@@ -2,26 +2,9 @@
 
 data d;
 
-void testRenogyReading(data &d)
-{
-  d.powerData->updateData();
-  d.powerData->printRenogyData();
-}
-
-void initJustPower(data &d)
-{
-  attachInterrupt(digitalPinToInterrupt(RX_INTERRUPT), commsHandler, RISING);
-  Serial1.begin(9600, SERIAL_8N1);
-  timerInit();
-  initMuxPins();
-  d.powerData = new powerInfo;
-  d.powerData->initData();
-}
-
 void setup()
 {
   initializeStartup(d);
-  // initJustPower(d);
 }
 
 void loop()
